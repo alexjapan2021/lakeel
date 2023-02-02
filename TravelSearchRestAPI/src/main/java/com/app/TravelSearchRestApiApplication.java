0cbc14efd5e78c946124a49a9b887068630c7401
alex <alexjapan2021@gmail.com>
2023-02-03 05:24:04 +0900

    Lakeel課題の開発

---
+++ TravelSearchRestAPI/src/main/java/com/app/TravelSearchRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSearchRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSearchRestApiApplication.class, args);
+	}
+
+}
---
+++ TravelSearchRestAPI/src/main/java/searchFormData/formsearch.java
@@ -0,0 +1,25 @@
+package searchFormData;
+
+public class formsearch {
+	
+	//公共交通機関経路検索
+	public static class publicTransportformInput{
+		public String departure;
+		public String destination;
+		public String sort;
+		public Integer count = 20;
+	}
+	
+	//周辺駅検索
+	public static class nearStaionformInput{
+		public String address;
+		public Integer radius = 1000;
+	}
+	
+	//徒歩距離検索
+	public static class walkingDistanceformInput{
+		public String origin;
+		public String destination;
+	}
+	
+}
