ed13e24aae7eb48bf0903a37a2fe43b3338f8668
alex <alexjapan2021@gmail.com>
2023-02-03 06:52:11 +0900

    デフォルト値の設定

--- TravelSearchRestAPI/src/main/java/searchFormData/formsearch.java
+++ TravelSearchRestAPI/src/main/java/searchFormData/formsearch.java
@@ -1,12 +1,15 @@
 package searchFormData;
 
+import lombok.Data;
+
+@Data
 public class formsearch {
 	
 	//公共交通機関経路検索
 	public static class publicTransportformInput{
 		public String departure;
 		public String destination;
-		public String sort;
+		public String sort = "time";
 		public Integer count = 20;
 	}
 	
